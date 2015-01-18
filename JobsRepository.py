7a4c08befbe7fdea1f825bf2e460d2bf92069ef8
orestes72 <orestes72@hotmail.com>
2015-01-18 09:35:04 -0500

    Add concept of company

--- JobsRepository.py
+++ JobsRepository.py
@@ -1,12 +1,15 @@
 __author__ = 'John'
 import sqlite3
-import re
 import urllib
 
 class JobsRepository:
     def __init__(self, filename="JobsDatabase.db"):
         self.conn = sqlite3.connect(filename)  #Once you have a Connection, you can create a Cursor object and call its execute() method to perform SQL commands:
         self.c = self.conn.cursor()
+
+    def deleteAllJobResultsOnDate(self, date):
+        self.c.execute('DELETE FROM JobsPerCityAndKeyword WHERE date = :date', {"date":date})
+        self.conn.commit()
 
     def getAllCities(self):
         cities = []
@@ -22,9 +25,21 @@ class JobsRepository:
             jobs.extend([job])
         return jobs
 
+    def getAllJobsPerCity(self, city, jobType):
+        results = []
+        for row in self.c.execute('select jc.date, c.cityname, c.stateInitials, j.keywordname, jc.numberofjobs from city c, jobkeyword j, jobspercityandkeyword jc where jc.cityid = c.cityid and jc.jobkeywordid = j.jobkeywordid and c.cityname = :city and j.keywordName = :keyWord  order by jc.date asc', {"city":city.cityName, "keyWord":jobType.keywordName}):
+            result = JobSearchModel(row[0], row[1], row[2], row[3], int(row[4]))
+            results.extend([result])
+        return results
+
     def insertOrUpdateJobResults(self, date, city, job, numJobs):
-        #self.c.execute('INSERT INTO JobsPerCityAndKeyword (date, cityId, jobKeywordId, numberOfJobs) VALUES (:date, :cityId, :jobKeywordId, :numberOfJobs)', {"date":date, "cityId":city.cityId, "jobKeywordId":job.jobKeywordId, "numberOfJobs":numJobs})
-        self.c.execute('IF NOT EXISTS (SELECT 1 FROM JobsPerCityAndKeyword WHERE date = :date AND cityId = :cityId and jobKeywordId = :jobKeywordId) BEGIN 	INSERT INTO JobsPerCityAndKeyword (date, cityId, jobKeywordId, numberOfJobs)  	VALUES (:date, :cityId, :jobKeywordId, :numberOfJobs) END ELSE  	UPDATE JobsPerCityAndKeyword SET numberOfJobs = :numberOfJobs WHERE date = :date AND cityId = :cityId and jobKeywordId = :jobKeywordId 	', {"date":date, "cityId":city.cityId, "jobKeywordId":job.jobKeywordId, "numberOfJobs":numJobs})
+        self.c.execute('INSERT INTO JobsPerCityAndKeyword (date, cityId, jobKeywordId, numberOfJobs) VALUES (:date, :cityId, :jobKeywordId, :numberOfJobs)', {"date":date, "cityId":city.cityId, "jobKeywordId":job.jobKeywordId, "numberOfJobs":numJobs})
+        #self.c.execute('IF NOT EXISTS (SELECT 1 FROM JobsPerCityAndKeyword WHERE date = :date AND cityId = :cityId and jobKeywordId = :jobKeywordId) BEGIN 	INSERT INTO JobsPerCityAndKeyword (date, cityId, jobKeywordId, numberOfJobs)  	VALUES (:date, :cityId, :jobKeywordId, :numberOfJobs) END ELSE  	UPDATE JobsPerCityAndKeyword SET numberOfJobs = :numberOfJobs WHERE date = :date AND cityId = :cityId and jobKeywordId = :jobKeywordId 	', {"date":date, "cityId":city.cityId, "jobKeywordId":job.jobKeywordId, "numberOfJobs":numJobs})
+        self.conn.commit()
+
+    def insertCompany(self, city, job, name):
+        self.c.execute('DELETE FROM Company WHERE cityId = :cityId AND jobKeywordId = :jobKeywordId AND companyName = :companyName', {"cityId":city.cityId, "jobKeywordId": job.jobKeywordId, "companyName":name})
+        self.c.execute('INSERT INTO Company (cityId, jobKeywordId, companyName) VALUES (:cityId, :jobKeywordId, :companyName)', {"cityId":city.cityId, "jobKeywordId": job.jobKeywordId, "companyName":name})
         self.conn.commit()
 
 
@@ -50,3 +65,12 @@ class JobModel:
         return str(self.jobKeywordId) + " " + self.keywordName
     def ToUrlString(self):
         return urllib.parse.quote_plus(self.keywordName)
+
+
+class JobSearchModel:
+    def __init__(self, date, cityName, stateInitials, keywordName, numberOfJobs):
+        self.date = date
+        self.cityName = cityName
+        self.stateInitials = stateInitials
+        self.keywordName = keywordName
+        self.numberOfJobs = numberOfJobs
--- Scraper.py
+++ Scraper.py
@@ -4,32 +4,43 @@ import ctypes  # for MessageBoxW
 import datetime
 import JobsRepository
 
+RESULTS_XPATH = '//div[@id="index-hed"]/h2/span/text()'
+
+
+
 curDate = datetime.datetime.now().date()
 range = 20
 
 repo = JobsRepository.JobsRepository("C:\\temp\\JobsDatabase.db")
 
+repo.deleteAllJobResultsOnDate(curDate)
 cities = repo.getAllCities()
 jobs = repo.getAllJobDescriptions()
 
-for city in cities:
-    print(city.ToUrlString())
-for job in jobs:
-    print(job.ToUrlString())
-
-
-def getNumberOfResultsForCityAndJob():
-    requestUrl = 'https://careers.stackoverflow.com/jobs?searchTerm=' + job.ToUrlString() + '&location=' + city.ToUrlString() + '&range=' + str(
-        range) + '&distanceUnits=Miles'
-    page = requests.get(requestUrl)
-    tree = html.fromstring(page.text)
-    resultsDiv = tree.xpath('//div[@id="index-hed"]/h2/span/text()')
+def getNumberOfResults(dom):
+    resultsDiv = dom.xpath('//div[@id="index-hed"]/h2/span/text()')
     return (resultsDiv[0].split(" ")[0])
 
+def getCompanyNames(dom):
+    companies = []
+    companyTags = dom.xpath('//strong[@class="-employer"]')
+    for company in companyTags:
+        companies.extend([company.text])
+    return (companies)
+
+def getDomTreeForCityAndJobSearch():
+    requestUrl = 'https://careers.stackoverflow.com/jobs?searchTerm=' + job.ToUrlString() + '&location=' + city.ToUrlString() + '&range=' + str(range) + '&distanceUnits=Miles'
+    page = requests.get(requestUrl)
+    tree = html.fromstring(page.text)
+    return tree
 
 for city in cities:
     for job in jobs:
-        jobsInCity = getNumberOfResultsForCityAndJob()
+        dom = getDomTreeForCityAndJobSearch()
+        jobsInCity = getNumberOfResults(dom)
         repo.insertOrUpdateJobResults(curDate, city, job, jobsInCity)
-
+        companies = getCompanyNames(dom)
+        for company in companies:
+            repo.insertCompany(city,job,company)
 repo.close()
+
